7239e48901cbb59408e5e7ae459d9f4cfc8fa25e
Jianwei Cui <thucjw@gmail.com>
2015-04-27 21:37:33 -0400

    improvement

--- src/mcc.v
+++ src/mcc.v
@@ -20,6 +20,7 @@
 `define TGT_MTX_ROWS 1024
 `define TGT_MTX_COLS 1024
 `define TGT_MTX_COLS_BIN 10
+`define TGT_MTX_ROWS_BIN 10
 module mcc(
    
  
@@ -38,7 +39,11 @@ module mcc(
     dac_out,
     dac_valid_out,
     dac_en,
+    
+    x_value_idx_in,
+    y_value_idx_in,
 
+    y_value_request,
     y_values_out,
     y_values_valid,
     mux_sel
@@ -54,19 +59,23 @@ module mcc(
     input rstn;
     input [`DATA_WIDTH - 1    : 0] x_values_in;
     input  x_values_valid_in;
-    input [`DATA_WIDTH - 1 : 0]     b_value_in;
+    input [`DATA_WIDTH - 1 : 0]    b_value_in;
     input [`XBAR_SIZE_BIN - 1 : 0] b_diag_in;
     input [`XBAR_SIZE_BIN - 1 : 0] b_offset_in;
     input block_valid_in;
     input new_diagonal;
     input dac_en;
 
-    output [`DATA_WIDTH * `XBAR_SIZE - 1 : 0] adc_in;
-    output adc_valid_in;
+    input [0 : `XBAR_SIZE * `TGT_MTX_COLS_BIN - 1] x_value_idx_in;
+    input [0 : `XBAR_SIZE * `TGT_MTX_ROWS_BIN - 1] y_value_idx_in;
+
+    input [0 : `DATA_WIDTH * `XBAR_SIZE - 1] adc_in;
+    input adc_valid_in;
     output [0 : `DATA_WIDTH * `XBAR_SIZE - 1] dac_out;
     output dac_valid_out;
 
-    output [`XBAR_SIZE * `DATA_WIDTH - 1 : 0] y_values_out;
+    input y_value_request;
+    output reg [`DATA_WIDTH - 1 : 0] y_values_out;
     output y_values_valid;
 
     output [`XBAR_SIZE_BIN * `XBAR_SIZE - 1 : 0] mux_sel;
@@ -74,6 +83,40 @@ module mcc(
     reg [`XBAR_SIZE * `DATA_WIDTH - 1 : 0] x_values_reg;
     
     reg [`DATA_WIDTH - 1 : 0] dac_data_reg [0: `XBAR_SIZE - 1];
+    reg [`DATA_WIDTH - 1 : 0] adc_data_reg [0: `XBAR_SIZE - 1];
+
+    reg [`TGT_MTX_COLS_BIN - 1 : 0] x_value_idx_reg[0 : `XBAR_SIZE - 1];
+    reg [`TGT_MTX_ROWS_BIN - 1 : 0] y_value_idx_reg[0 : `XBAR_SIZE - 1];
+    
+ 
+  genvar j; 
+  generate
+  for(j = 0; j < `XBAR_SIZE; j = j + 1) begin:x_value_idx
+    always @ (posedge clk or negedge rstn) begin
+        if(!rstn)
+            x_value_idx_reg[j] <=0 ;
+        else
+            x_value_idx_reg[j] <= x_value_idx_in[j * `TGT_MTX_COLS_BIN : j * `TGT_MTX_COLS_BIN + `TGT_MTX_COLS_BIN - 1];
+    end
+
+  end
+  endgenerate
+
+ 
+  genvar j; 
+  generate
+  for(j = 0; j < `XBAR_SIZE; j = j + 1) begin:y_value_idx
+    always @ (posedge clk or negedge rstn) begin
+        if(!rstn)
+            y_value_idx_reg[j] <=0 ;
+        else
+            y_value_idx_reg[j] <= y_value_idx_in[j * `TGT_MTX_ROWS_BIN : j * `TGT_MTX_ROWS_BIN + `TGT_MTX_ROWS_BIN - 1];
+    end
+
+  end
+  endgenerate
+
+
 
     always@(posedge clk or negedge rstn) begin
         if(rstn == 0)
@@ -114,6 +157,95 @@ module mcc(
         assign dac_out[i * `DATA_WIDTH : i * `DATA_WIDTH + `DATA_WIDTH - 1] = dac_en ? dac_data_reg[i] : 0;
     end
     endgenerate
+
+    
+    
+  genvar j; 
+  generate
+  for(j = 0; j < `XBAR_SIZE; j = j + 1) begin:adc
+    always @ (posedge clk or negedge rstn) begin
+        if(!rstn)
+            adc_data_reg[j] <=0 ;
+        else
+            adc_data_reg[j] <= adc_in[j * `DATA_WIDTH : j * `DATA_WIDTH + `DATA_WIDTH - 1];
+    end
+
+  end
+  endgenerate
+
+  wire [`DATA_WIDTH - 1 : 0] sel_internal_y[0 : `XBAR_SIZE - 1];
+
+  generate
+  for(j = 0; j < `XBAR_SIZE; j = j + 1) begin:sel_internal_y_loop
+      assign sel_internal_y[j] = internal_y[y_value_idx_reg[j]]; 
+  end
+  endgenerate
+    
+  reg [`DATA_WIDTH - 1 : 0] y_sub[0 : `XBAR_SIZE - 1];
+
+  generate
+  for(j = 0; j < `XBAR_SIZE; j = j + 1) begin:y_sub_loop
+     always @ (posedge clk or negedge rstn) begin
+         if(!rstn)
+            y_sub[j] <= 0;
+         else
+            y_sub[j] <= adc_data_reg[j] + sel_internal_y[j];
+     end
+  end    
+  endgenerate
+
+  reg [`XBAR_SIZE_BIN - 1: 0] y_sub_counter;
+  always @ (posedge clk or negedge rstn) begin
+      if(!rstn)
+          y_sub_counter = 0;
+      else
+          //if(write_back_en)
+              y_sub_counter = y_sub_counter + 1;
+  end
+
+  always @ (posedge clk or negedge rstn) begin
+      if(!rstn)
+          
+          internal_y[y_value_idx_reg[y_sub_counter]] = 0;
+      else
+          internal_y[y_value_idx_reg[y_sub_counter]] = y_sub[y_sub_counter];
+
+  end
+  /*
+  genvar j;
+  genvar k;
+  generate
+  for(j = 0; j < `TGT_MTX_ROWS; j = j + 1) begin:internal_y_loop
+  always @ (posedge clk or negedge rstn) begin
+      if(!rstn)
+          internal_y[j] =  0;
+      else
+          generate
+          
+          internal_y[j] =  ;
+      
+  end
+  end
+  endgenerate
+  */
+
+
+  reg [`TGT_MTX_ROWS_BIN - 1 : 0]  mtx_y_value_out_count;
+  always@(posedge clk or negedge rstn) begin
+      if(!rstn)
+          mtx_y_value_out_count <= 0;
+      else
+          if(y_value_request == 1'b1)
+              mtx_y_value_out_count <= mtx_y_value_out_count + 1;
+  end
+
+ always@(posedge clk or negedge rstn) begin
+      if(!rstn)
+         y_values_out <= 0;
+      else
+         y_values_out <= internal_y[mtx_y_value_out_count];
+  end
+
 
 
 endmodule
